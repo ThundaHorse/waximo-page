d25d25892f60a6f283e24846bc3cdc1c1024a740
Abraham Kim <abeeeeekim@gmail.com>
2024-10-25 09:14:50 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,6 @@ import EmailSignupComponent from './components/EmailSignupComponent';
 import './index';
 
 function App() {
-  function onSubmit() {
-    console.log('fdsa');
-
-    fetch('https://api.ipdata.co')
-      .then((response) => {
-        return response.json();
-        // @ts-ignore
-      }, 'jsonp')
-      .then((res) => {
-        // @ts-ignore
-        console.log(res.ip);
-      })
-      .catch((err) => console.log(err));
-  }
-
   return (
     <div className='App container mx-auto flex flex-col h-screen'>
       <div className='flex-grow mb-auto'>
