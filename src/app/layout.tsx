83349eea0dec1109c4baa0de28dafa42c2e46f7f
Abraham Kim <abeeeeekim@gmail.com>
2024-12-06 14:40:26 -0500

    remove tag manager temp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,13 +43,7 @@ export default function RootLayout({
           content='Coming Soon!'
         />
       </head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSN07QK9R2"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-LSN07QK9R2');
-        </script>
+
       <body>{children}</body>
     </html>
   );
